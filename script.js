65451ee8527aa1252b6b5ea1f57e67b2327f6f95
Cwell21 <cwe11@yahoo.com>
2018-05-24 20:17:38 -0600

    Step 3, header is now a component

--- script.js
+++ script.js
@@ -17,6 +17,12 @@ var gators = [
   }
 ]
 
+
+Vue.component('app-header', {
+  props: ['text'],
+  template: '<header><h1><img src="alligator-logo.png" alt="AlligatorLogo">{{ text }}</h1></header>'  
+})
+
 var app = new Vue({
   el: '#app',
   data: {
